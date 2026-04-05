238e84978cd5693b440fad7ab36513c09c1078d6
BananoDurisimo <vicenterios.vargas007@gmail.com>
2026-04-05 14:04:43 -0500

    m

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 const dotenv = require('dotenv')
-const connectDB = require('./config/db.js')
+const connectDB = require('./Config/db.js')
 const useRoutes = require('./Routes/user.routes')
 
 
